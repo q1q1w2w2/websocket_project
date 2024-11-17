760b17fd852422f15d9335f57c5312cae7662bee
q1q1w2w2 <xmrrhdwjdqls@gmail.com>
2024-11-17 10:46:03 +0900

    line

--- src/main/java/com/example/websocket/gpt/web/GptController.java
+++ src/main/java/com/example/websocket/gpt/web/GptController.java
@@ -28,7 +28,11 @@ public class GptController {
     public String chat(@RequestParam("prompt") String prompt) {
         log.info("prompt: {}", prompt);
 
-        GptRequest request = new GptRequest(model,  "채팅으로 대화 중이야. 대화 중 \'"+ prompt + "\'라는 답장을 받았을 때 어떻게 답장하는 것이 좋을지에 대해 짧게 답변해줘.", 1, 64, 1, 2, 2);
+        GptRequest request = new GptRequest(
+                model,
+                "채팅으로 대화 중이야. 대화 중 \'"+ prompt + "\'라는 답장을 받았을 때 어떻게 답장하는 것이 좋을지에 대해 짧게 답변해줘.",
+                1, 64, 1, 2, 2
+        );
         GptResponse gptResponse = restTemplate.postForObject(apiUrl, request, GptResponse.class);
 
         log.info("request: {}", request);
